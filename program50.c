1a3254c96648942cad3130d94a46cbbd908ba1a8
MahajanKunal <117641294+MahajanKunal@users.noreply.github.com>
2023-03-11 19:04:11 +0530

    Add files via upload

---
+++ program50.c
@@ -0,0 +1,34 @@
+// COUNT DIGIT
+
+#include<stdio.h>
+
+int SumDigits(int iNo)
+{
+    int iDigit= 0;
+    int iSum = 0;
+
+    while(iNo != 0)
+    {
+        iDigit = iNo%10;
+        printf("%d\n",iDigit);
+        iSum = iSum +iDigit;
+        iNo = iNo/10;
+    }
+    return iSum;
+}
+int main()
+{
+    int iValue = 0;
+    int iRet = 0;
+
+    printf("Enter the number \n");
+
+    scanf("%d",&iValue);
+
+    iRet = SumDigits(iValue);
+
+    printf("sumetion of the digit is %d",iRet);
+
+
+    return 0;
+}
